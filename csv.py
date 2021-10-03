021f29572ae9f6298e8e0910eed57f0b6f4e5de4
Giang Nguyen <59216441+giangngt@users.noreply.github.com>
2021-10-03 10:10:57 +0700

    Update csv

--- csv.py
+++ csv.py
@@ -1,21 +1,25 @@
 #!pip install pandas
 #!pip install requests
 
-import requests
 import pandas as pd
 
-# Write your code here
-url = "http://api.exchangeratesapi.io/v1/latest?base=EUR&access_key=xxx" 
-html = requests.get(url)
-a = html.json()
+url ='https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0101EN-SkillsNetwork/labs/Module%205/data/addresses.csv'
+df = pd.read_csv(url)
 
-# Turn the data into a dataframe
-df = pd.DataFrame(a)
-df = df.rename(columns={'rates': 'Rates'})
+#Add column names
+df.columns =['First Name', 'Last Name', 'Location ', 'City','State','Area Code']
 
+# To select the first row
+df.loc[0]
 
-# Drop unnescessary columns
-df = df.drop(['success', 'timestamp', 'base', 'date'], 1)
+# To select the 0th,1st and 2nd row of "First Name" column only
+df.loc[[0,1,2], "First Name" ]
 
-# Save the Dataframe
-df.to_csv('exchange_rates_1.csv')
+# To select the 0th,1st and 2nd row of "First Name" column only
+df.iloc[[0,1,2], 0]
+
+# Add 10 to each element
+df = df.transform(func = lambda x : x + 10)
+
+# Find sqrt
+result = df.transform(func = ['sqrt'])
